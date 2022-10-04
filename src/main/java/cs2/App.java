7e5ca1f07006073ec3c826fd57cc9d8775c2638f
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-10-03 23:19:49 -0400

    test commit from intellij

--- src/main/java/cs2/App.java
+++ src/main/java/cs2/App.java
@@ -1,18 +1,15 @@
 package cs2;
 //import necessary libraries
-import java.time.LocalDate; 
-import javafx.scene.paint.Color;
+import java.time.LocalDate;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.Popup;
 import javafx.application.Application;
 import javafx.scene.control.MenuItem;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.paint.Color;
 
 
 public class App extends Application
@@ -23,12 +20,12 @@ public class App extends Application
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("User Interface"); //set the title to "User Interface Test"
         final MenuBar menuBar = new MenuBar(); //creates a menu bar at the top of the window
         Menu Options = new Menu("Options"); //add new menu called "Options"
         
-
+        primaryStage.show();
 
         //add menu items for m1
         MenuItem OptionsItem1 = new MenuItem("Date"); 
@@ -38,49 +35,30 @@ public class App extends Application
         Options.getItems().addAll(OptionsItem1, OptionsItem2, OptionsItem3, OptionsItem4);
 
         LocalDate currentDate = LocalDate.now();
-        Label popupLabel = new Label("Current Date" + currentDate);
-        Popup datePopup = new Popup();
+        Label popupLabel = new Label("Current Date: " + currentDate);
+
+        final Popup datePopup = new Popup();
         datePopup.getContent().add(popupLabel);
 
         
 
-        OptionsItem1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                
-                
-                if (!datePopup.isShowing()){
-                    datePopup.show(primaryStage);
-                }
-                else datePopup.hide();
+        OptionsItem1.setOnAction(event -> {
+
+            if (!datePopup.isShowing()){
+                datePopup.show(primaryStage);
             }
+            else datePopup.hide();
         });
 
-        OptionsItem2.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event)
-            {
-                System.out.println("hello");
-            };
-        });
+        OptionsItem2.setOnAction(event -> System.out.println("hello"));
 
-        OptionsItem3.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event)
-            {
-                System.out.println("bonjour");
-            };
-        });
+        OptionsItem3.setOnAction(event -> System.out.println("bonjour"));
 
-        OptionsItem4.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event)
-            {
-                System.out.println("bonjour");
-            };
-        });
+        OptionsItem4.setOnAction(event -> System.out.println("bonjour"));
 
         
         menuBar.getMenus().addAll(Options); 
-        Scene scene = new Scene(menuBar, 600, 600); 
+        Scene scene = new Scene(menuBar, 600, 600);
 
         primaryStage.setScene(scene);
         
