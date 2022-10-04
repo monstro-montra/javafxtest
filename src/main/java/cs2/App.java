59ae72472b2f9fe73cc56a8cbe50c8db89515fbb
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-10-04 14:03:40 -0400

    Added functionality to switch 'screens' while keeping the menu bar by creating separate classes for each node.

--- src/main/java/cs2/App.java
+++ src/main/java/cs2/App.java
@@ -1,106 +1,129 @@
 package cs2;
 //import necessary libraries
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.Popup;
 import javafx.application.Application;
-import javafx.scene.paint.Color;
 
 import static java.lang.System.out;
 
 
 public class App extends Application
 {
+    //class level variables
+    private Stage stage = new Stage();
+    private final MenuItem OptionsItem1 = new MenuItem("Date");
+    private final MenuItem OptionsItem2 = new MenuItem("Write");
+    private final MenuItem OptionsItem3 = new MenuItem("Option 3");
+    private final MenuItem OptionsItem4 = new MenuItem("Option 4");
+    private final MenuBar menuBar = new MenuBar(); // creates a menu bar at the top of the window
+    private final Menu Options = new Menu("Options"); //add new menu called "Options"
+    private final Node Node1 = new NodeOne();
+    private final Node Node2 = new NodeTwo();
+    private BorderPane root;
+
+    VBox vBox;
+    @Override
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
+        primaryStage.setTitle("User Interface"); //set the title to "User Interface Test"
+        genMenu();
+
+        root = new BorderPane();
+        root.setTop(menuBar);
+
+        Scene mainScene = new Scene(root, 600, 600);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
+
+    }
+
+    private void genMenu(){
+        //add menu items for Options Menu
+        Options.getItems().addAll(OptionsItem1, OptionsItem2, OptionsItem3, OptionsItem4);
+        //set action event for OptionsItem1
+        OptionsItem1.setOnAction(event -> {
+            switchView(Node1);
+
+        });
+        //set action event for OptionItem2
+        OptionsItem2.setOnAction(event -> {
+            switchView(Node2);
+
+        });
+        OptionsItem3.setOnAction(event -> {
+            out.println("bonjour");
+        });
+
+        OptionsItem4.setOnAction(event -> {
+            out.println("bonjour");
+        });
+
+        vBox = new VBox(menuBar);
+        menuBar.getMenus().addAll(Options);
+    }
+
+    private void genSceneOne(){
+
+    }
     public static void main( String[] args )
     {
         launch(args);
     }
 
-    @Override
-    public void start(final Stage primaryStage) throws Exception {
-        primaryStage.setTitle("User Interface"); //set the title to "User Interface Test"
-        final MenuBar menuBar = new MenuBar(); //creates a menu bar at the top of the window
-        Menu Options = new Menu("Options"); //add new menu called "Options"
 
-        //add menu items for Options Menu
-        MenuItem OptionsItem1 = new MenuItem("Date");
-        MenuItem OptionsItem2 = new MenuItem("Write");
-        MenuItem OptionsItem3 = new MenuItem("Option 3");
-        MenuItem OptionsItem4 = new MenuItem("Option 4");
-        Options.getItems().addAll(OptionsItem1, OptionsItem2, OptionsItem3, OptionsItem4);
 
-        //create pop up using a label which contains the current date and time. this will be used for OptionsItem1
+
+    private void switchView(Node view){
+        root.setCenter(view);
+
+    }
+}
+
+class NodeOne extends StackPane {
+    public NodeOne(){
         LocalDate currentDate = LocalDate.now();
         LocalTime currentTime = LocalTime.now();
-        Label popupLabel = new Label("Current Date: " + currentDate + "\n" + "Current Time: " + currentTime);
-        final Popup datePopup = new Popup();
-        datePopup.getContent().add(popupLabel);//add popupLabel to the datePopup
-
-        //set action event for OptionsItem1
-        OptionsItem1.setOnAction(event -> {
-            if (!datePopup.isShowing()){ //if datePopup is not showing
-                datePopup.show(primaryStage); //show datePopup
-            }
-            else datePopup.hide(); //otherwise, hide datePopup
-        });
-
-
-        TextField writeTextField = new TextField();
-        writeTextField.setOnAction(event -> {
-            String userInput = writeTextField.getText();
-            setTextArea(userInput);
-
-        });
-        final Popup writePopup = new Popup();
-        writePopup.getContent().add(writeTextField);
-
-        //set action event for OptionItem2
-        OptionsItem2.setOnAction(event -> {
-            if (!writePopup.isShowing()){ //if datePopup is not showing
-                writePopup.show(primaryStage); //show datePopup
-            }
-            else writePopup.hide(); //otherwise, hide datePopup
-        });
-
-
-        OptionsItem3.setOnAction(event -> out.println("bonjour"));
-
-        OptionsItem4.setOnAction(event -> out.println("bonjour"));
-
-
-        menuBar.getMenus().addAll(Options);
-        Scene scene = new Scene(menuBar, 600, 600);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        setStyle("-fx-background-color: lightblue; -fx-font-size: 30px;");
+        Label showDateAndTime = new Label("Current Date: " + currentDate + "\n" + "Current Time: " + currentTime);
+        getChildren().add(showDateAndTime);
 
     }
 
-    public static void setTextArea(String fileName) {
+}
 
-        String line;
-        String content = null;
+class NodeTwo extends StackPane {
+    public NodeTwo(){
+        setStyle("-fx-background-color: red; -fx-font-size: 30px;");
+        TextField userTextField = new TextField();
+
+        userTextField.setOnAction(event -> {
+            String userInput = userTextField.getText();
+            setTextArea(userInput);
+
+        });
+        getChildren().add(userTextField);
+
+    }
+    public static void setTextArea(String userInput) {
 
         try {
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader buffer = new   BufferedReader(fileReader);
-
-            while ((line = buffer.readLine()) != null) {
-                out.println(line);
-                content += line;
-            }
-            buffer.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Output.txt"));
+            writer.write(userInput);
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
