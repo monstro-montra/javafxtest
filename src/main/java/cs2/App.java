2e1043437ff2d454ccc12cfe905ce53c26cc6cec
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-10-05 00:20:42 -0400

    just overall fix this man lol

--- src/main/java/cs2/App.java
+++ src/main/java/cs2/App.java
@@ -5,12 +5,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicReference;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.application.Application;
 
@@ -27,21 +28,26 @@ public class App extends Application
     private final MenuItem OptionsItem4 = new MenuItem("Option 4");
     private final MenuBar menuBar = new MenuBar(); // creates a menu bar at the top of the window
     private final Menu Options = new Menu("Options"); //add new menu called "Options"
-    private final Node Node1 = new NodeOne();
-    private final Node Node2 = new NodeTwo();
-    private BorderPane root;
+    private BorderPane borderPane;
 
-    VBox vBox;
+    GridPane view1 = new GridPane();
+    GridPane view2 = new GridPane();
+    GridPane view3 = new GridPane();
+    GridPane view4 = new GridPane();
+    VBox menuVBox;
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
         primaryStage.setTitle("User Interface"); //set the title to "User Interface Test"
         genMenu();
+        genPaneOne();
+        genPaneTwo();
 
-        root = new BorderPane();
-        root.setTop(menuBar);
+        borderPane = new BorderPane();
+        borderPane.setTop(menuBar);
 
-        Scene mainScene = new Scene(root, 600, 600);
+
+        Scene mainScene = new Scene(borderPane, 600, 600);
         primaryStage.setScene(mainScene);
         primaryStage.show();
 
@@ -53,12 +59,12 @@ public class App extends Application
         Options.getItems().addAll(OptionsItem1, OptionsItem2, OptionsItem3, OptionsItem4);
         //set action event for OptionsItem1
         OptionsItem1.setOnAction(event -> {
-            switchView(Node1);
+            switchPane(view1);
 
         });
         //set action event for OptionItem2
         OptionsItem2.setOnAction(event -> {
-            switchView(Node2);
+            switchPane(view2);
 
         });
         OptionsItem3.setOnAction(event -> {
@@ -69,12 +75,37 @@ public class App extends Application
             out.println("bonjour");
         });
 
-        vBox = new VBox(menuBar);
+        menuVBox = new VBox(menuBar);
         menuBar.getMenus().addAll(Options);
     }
 
-    private void genSceneOne(){
+    private GridPane genPaneOne(){
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+        Label showDateAndTime;
+        showDateAndTime = new Label("Current Date: " + currentDate + "\n" + "Current Time: " + currentTime);
 
+        view1.getChildren().addAll(showDateAndTime);
+        view1.setStyle("-fx-border-color:red; -fx-border-width: 600 600 2 2");
+        view1.setRowIndex(showDateAndTime, 1);
+        view1.setAlignment(Pos.TOP_CENTER);
+
+
+        return view1;
+    }
+
+    private GridPane genPaneTwo(){
+        TextField userTextField = new TextField();
+        userTextField.setMaxWidth(300);
+
+        Button submit = new Button("Submit");
+        view2.getChildren().addAll(userTextField, submit);
+
+        view2.setHalignment(userTextField, HPos.CENTER);
+        view2.setHalignment(submit, HPos.RIGHT);
+        view2.setStyle("-fx-background-color: blue");
+
+        return view2;
     }
     public static void main( String[] args )
     {
@@ -84,49 +115,45 @@ public class App extends Application
 
 
 
-    private void switchView(Node view){
-        root.setCenter(view);
-
-    }
-}
-
-class NodeOne extends StackPane {
-    public NodeOne(){
-        LocalDate currentDate = LocalDate.now();
-        LocalTime currentTime = LocalTime.now();
-        setStyle("-fx-background-color: lightblue; -fx-font-size: 30px;");
-        Label showDateAndTime = new Label("Current Date: " + currentDate + "\n" + "Current Time: " + currentTime);
-        Button submit = new Button("Submit");
-        getChildren().addAll(showDateAndTime, submit);
-
+    private void switchPane(Pane pane){
+        borderPane.setCenter(pane);
 
     }
 
-}
-
-class NodeTwo extends StackPane {
-    public NodeTwo(){
-        setStyle("-fx-background-color: red; -fx-font-size: 30px;");
-        TextField userTextField = new TextField();
-        userTextField.setMaxWidth(300);
-
-        userTextField.setOnAction(event -> {
-            String userInput = userTextField.getText();
-            setTextArea(userInput);
-
-        });
-        getChildren().add(userTextField);
-
-    }
     public static void setTextArea(String userInput) {
 
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("Output.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt"));
             writer.write(userInput);
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+}
+
+
+
+/*
+class NodeTwo extends StackPane {
+    public NodeTwo(){
+        setStyle("-fx-background-color: red; -fx-font-size: 30px;");
+        TextField userTextField = new TextField();
+        userTextField.setMaxWidth(300);
+        userTextField.setAlignment(Pos.TOP_CENTER);
+        Button submit = new Button("Submit");
+        submit.setAlignment(Pos.BOTTOM_CENTER);
+
+        userTextField.setOnAction(event -> {
+            String userInput = userTextField.getText();
+            setTextArea(userInput);
+
+        });
+
+        getChildren().addAll(userTextField, submit);
+
+    }
+
 
 }
+*/
